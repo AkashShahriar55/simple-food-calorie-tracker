b1fcb87c281456c411f397fb476172c4ef2e7d73
Akash Shahriar <akashshahriar55@gmail.com>
2022-10-14 16:39:39 +0600

    android client app inititated

---
+++ Calorietracker/app/src/main/java/com/akash/calorie_tracker/CalorieTrackerApplication.kt
@@ -0,0 +1,12 @@
+package com.akash.calorie_tracker
+
+import android.app.Application
+
+class CalorieTrackerApplication:Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
